ec3bb92670074627aa9b07189a82b7e2908d14d1
shougo.gotou <shogotoo0306@yahoo.co.jp>
2020-03-12 08:44:16 +0000

    follow, unfollow

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,7 +3,7 @@ class MicropostsController < ApplicationController
   before_action :correct_user, only: [:destroy]
   
   def create
-     @micropost = current_user.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
@@ -15,7 +15,7 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
-    @micropost.destroy
+  @micropost.destroy
     flash[:success] = 'メッセージを削除しました。'
     redirect_back(fallback_location: root_path)
   end
@@ -23,11 +23,11 @@ class MicropostsController < ApplicationController
   private
 
   def micropost_params
-    params.require(:micropost).permit(:content)
+     params.require(:micropost).permit(:content)
   end
   
   def correct_user
-    @micropost = current_user.microposts.find_by(id: params[:id])
+     @micropost = current_user.microposts.find_by(id: params[:id])
     unless @micropost
       redirect_to root_url
     end
